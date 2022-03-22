1b04cf6df4b4b23836a3e4e0a59714f8fb36b428
pushy <80036867+dahlah270@users.noreply.github.com>
2022-03-22 11:08:34 +0700

    Create virus.sh

---
+++ virus.sh
@@ -0,0 +1,74 @@
+ 
+ ​#!​/bin/sh 
+  
+ ​green=​'​\033[0;32m​' 
+ ​greenlight=​'​\033[32;1m​' 
+ ​red=​'​\033[31;1m​' 
+ ​white=​'​\033[37;1m​' 
+ ​black=​'​\033[30;1m​' 
+ ​yellow=​'​\033[33;1m​' 
+ ​echo​ ​$black​ ​"​==================================​" 
+ ​echo​ ​$red​   ​"​Author : Pushy" 
+ ​echo​ ​$red​   ​"​Team : Pushy Communitg​" 
+ ​echo​ ​$red​   ​"​Discord : PushyGamertag#7165​" 
+ ​echo​ ​$black​ ​"​====================================​" 
+ ​echo​ ​$black​ ​"​====================================​" 
+ ​echo​ ​$yellow​ ​"​1). Link-Virus​" 
+ ​echo​ ​$yellow​ ​"​2). Link-GC​" 
+ ​echo​ ​$yellow​ ​"​3). Exit​" 
+ ​echo​ ​$black​ ​"​====================================​" 
+ ​read​ -p ​"​Jangan Lupa Pilih :​"​ santai 
+  
+ ​if​ [ ​$santai​ ​=​ ​"​1​"​ ] 
+ ​then 
+ ​echo​ ​$black​ ​"​====================================​" 
+ ​echo​ ​$green​ ​"​1). Link Virus TROJANS​" 
+ ​echo​ ​$green​ ​"​2). Link Virus EXE​" 
+ ​echo​ ​$green​ ​"​3). Link Virus PC​" 
+ ​echo​ ​$black​ ​"​=====================================​" 
+ ​read​ -p ​"​Pilih Link Virusnya :​"​ saha 
+  
+ ​fi 
+  
+ ​if​ [ ​$saha​ ​=​ ​"​1​"​ ] 
+ ​then 
+ ​echo​ ​$yellow​ ​"​Sedangg Menunggu.....​" 
+ ​echo​ ​$yellow​ ​"​Sipp, Maaf Menunggu...​" 
+ ​echo​ ​$yellow​ ​"​Link :​"​ ​"​https://www.mediafire.com/file/btffebdhkry46s0​" 
+ ​echo​ ​$yellow​ ​"​Maaf, Telah Menunggu​" 
+  
+ ​fi 
+  
+ ​if​ [ ​$saha​ ​=​ ​"​2​"​ ] 
+ ​then 
+ ​echo​ ​$yellow​ ​"​Sedang Menunggu.....​" 
+ ​echo​ ​$yellow​ ​"​Sipp, Maaf Menunggu...​" 
+ ​echo​ ​$yellow​ ​"​Link :​"​ ​"​https://www.mediafire.com/file/kiwpmbv0cxsvqhu​" 
+ ​echo​ ​$yellow​ ​"​Maaf, Telah Menunggu​" 
+  
+ ​fi 
+  
+ ​if​ [ ​$saha​ ​=​ ​"​3​"​ ] 
+ ​then 
+ ​echo​ ​$yellow​ ​"​Sedang, Menunggu......​" 
+ ​echo​ ​$yellow​ ​"​Sipp, Maaf Menunggu...​" 
+ ​echo​ ​$yellow​ ​"​Link :​"​ ​"​https://www.mediafire.com/file/btffebdhkry46s0​" 
+ ​echo​ ​$yellow​ ​"​Maaf, Menunggu...​" 
+  
+ ​fi 
+  
+ ​if​ [ ​$santai​ ​=​ ​"​2​"​ ] 
+ ​then 
+ ​echo​ ​$white​ "https://discord.gg/fUVBBsv76H" 
+ ​echo​ ​$white​ ​"​Silakan Join Link GCnya​" 
+ ​echo​ ​$white​ ​"​Thanks​" 
+  
+ ​fi 
+  
+ ​if​ [ ​$santai​ ​=​ ​"​3​"​ ] 
+ ​then 
+ ​figlet Selamat 
+ ​figlet Tinggal 
+ ​exit 
+  
+ ​fi
